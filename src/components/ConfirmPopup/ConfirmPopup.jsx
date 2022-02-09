3c61f528cc41860f399818a20c173934ab40c712
Andrey Burlakov <andrburl2@yandex.ru>
2022-02-09 13:59:11 +0300

    Исправил ошибки, обновил пакеты

--- src/components/ConfirmPopup/ConfirmPopup.jsx
+++ src/components/ConfirmPopup/ConfirmPopup.jsx
@@ -5,8 +5,8 @@ class ConfirmPopup extends PureComponent {
   constructor(props) {
     super(props);
 
-    this.confirmButton = React.createRef();   // кнопка подтверждения
-    this.canselButton = React.createRef();    // кнопка отмены
+    this.confirmButton = React.createRef(); // кнопка подтверждения
+    this.cancelButton = React.createRef();  // кнопка отмены
   }
 
   createPromise = async () => {
@@ -16,7 +16,7 @@ class ConfirmPopup extends PureComponent {
         resolve(true)
       });
 
-      this.canselButton.current.addEventListener('click', () => {
+      this.cancelButton.current.addEventListener('click', () => {
         resolve(false)
       });
     })
@@ -31,7 +31,7 @@ class ConfirmPopup extends PureComponent {
           <h2 className='confirm__title'>{title}</h2>
 
           <div className='confirm__button-row'>
-            <button className='button confirm__button confirm__button_type_cancel' onClick={onClose} ref={this.canselButton}>Назад</button>
+            <button className='button confirm__button confirm__button_type_cancel' onClick={onClose} ref={this.cancelButton}>Назад</button>
             <button className='button confirm__button confirm__button_type_confirm' onClick={onClose} ref={this.confirmButton}>Подтвердить</button>
           </div>
         </div>
